7623ee7991c501314d6f20935a7448f5424be73b
Shayne Preston <prests@rpi.edu>
2018-12-01 13:32:36 -0500

    HMAC

---
+++ mac.py
@@ -0,0 +1,22 @@
+import hash
+
+def HMAC(k, m):
+    blockSize = 64 #for SHA-1
+    outerPad = '01011100'
+    innerPad = '00110110'
+
+    if(len(k) > blockSize):
+        hash.sha(k)
+    
+    while(len(k) < blockSize):
+        k += '0'
+    
+    while(len(outerPad) < blockSize and len(innerPad) < blockSize):
+        outerPad += outerPad
+        innerPad += innerPad
+    
+    outerPadKey = k ^ outerPad
+    innerPadKey = k ^ innerPad
+
+    hMAC = hash.sha(outerPadKey + hash.sha(innerPadKey + m))
+    return hMAC
