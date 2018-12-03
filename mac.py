8066c6fbce5d039ad8b1fdab675846d705eb9b5d
Shayne Preston <prests@rpi.edu>
2018-12-03 15:57:30 -0500

    key stuff

--- mac.py
+++ mac.py
@@ -1,22 +1,16 @@
-import hash
+import sha1
 
 def HMAC(k, m):
     blockSize = 64 #for SHA-1
-    outerPad = '01011100'
-    innerPad = '00110110'
+    outerPad = '0101110001011100010111000101110001011100010111000101110001011100'
+    innerPad = '0011011000110110001101100011011000110110001101100011011000110110'
 
     if(len(k) > blockSize):
-        hash.sha(k)
-    
-    while(len(k) < blockSize):
-        k += '0'
-    
-    while(len(outerPad) < blockSize and len(innerPad) < blockSize):
-        outerPad += outerPad
-        innerPad += innerPad
+        sha1.sha(k)
+    while outerPad < blockSize and innerPad < blockSize
     
     outerPadKey = k ^ outerPad
     innerPadKey = k ^ innerPad
 
-    hMAC = hash.sha(outerPadKey + hash.sha(innerPadKey + m))
+    hMAC = sha1.sha(outerPadKey + sha1.sh(innerPadKey + m))
     return hMAC
